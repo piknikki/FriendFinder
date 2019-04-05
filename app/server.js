cecb060b17f0f90542c4009eff5f91049da84533
Nikki Petersen <piknik@me.com>
2019-04-05 13:17:07 -0600

    update before heroku-ing

--- app/server.js
+++ app/server.js
@@ -1,2 +1,29 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const bodyParser = require("body-parser"); // parse the data in form
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true})) // for getting info from html form
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(req, res) {
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname + "/public/home.html"))
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/survey.html"));
+});
+
+app.post("/survey", function(req, res) {
+    var userResponse = req.body.submit;
+
+    console.log(userResponse);
+
+});
+
+
+
+app.listen(3000, function() {
+    console.log("server is running on port 3000")
+});
